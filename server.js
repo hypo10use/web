f43c06f3ed874a60f1f9026e5119adb39454f253
Rachel Waterman <rwaterman93@googlemail.com>
2021-10-08 21:40:32 +0100

    Changes for Heroku intergration.

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+app.use(express.static(__dirname + '/dist/ergohack'));
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/dist/ergohack/index.html'));
+});
+app.listen(process.env.PORT || 8080);
